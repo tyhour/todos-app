3e7af4cd8e891c663b743ea714a04c4f9742275a
tyhour <phaityhour@gmail.com>
2023-10-11 14:58:57 +0700

    Update Cors

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["sequelize"],
   },
+  serverRuntimeConfig: {
+    // Custom server settings
+    cors: {
+      // Define CORS options here
+      origin:
+        "https://stackblitzstarterskvefgd-ygqz--3000--7a1fe16d.local-corp.webcontainer.io", // Replace with the origin you want to allow
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true, // You may need this if your requests include credentials (e.g., cookies)
+    },
+  },
 };
 
 module.exports = nextConfig;
